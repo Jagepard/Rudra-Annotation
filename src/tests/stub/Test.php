886e50ecd572e40c65884115aa0799aa90ce2217
Jagepard <dankorot@gmail.com>
2017-03-27 18:31:56 +0300

    Также подправил под php 7.1

--- src/tests/stub/Test.php
+++ src/tests/stub/Test.php
@@ -1,10 +1,14 @@
 <?php
 
+declare(strict_types = 1);
+
 /**
  * @Routing(url = '')
  * @Defaults(name='user1'| lastname = 'sample'| age='0'| address = {country : 'Russia', state : 'Tambov'}| phone = '000-00000000')
  * @assertResult(false)
  * @Validate(name = 'min:150'| phone = 'max:9')
+ *
+ * Class Test
  */
 class Test
 {
@@ -19,5 +23,4 @@ class Test
     {
 
     }
-
 }
