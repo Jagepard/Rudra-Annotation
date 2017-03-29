08dfa4a7e3ec079b69084d2705bde7291d22f69c
Jagepard <dankorot@gmail.com>
2017-03-29 19:21:53 +0300

    Добавляет strict_types, необязательный в данном случае, просто для единообразия

--- src/tests/bootstrap.php
+++ src/tests/bootstrap.php
@@ -1,15 +1,7 @@
 <?php
 
-/**
- * Date: 17.02.17
- * Time: 14:25
- *
- * @author    : Korotkov Danila <dankorot@gmail.com>
- * @copyright Copyright (c) 2016, Korotkov Danila
- * @license   http://www.gnu.org/licenses/gpl.html GNU GPLv3.0
- */
+declare(strict_types = 1);
 
 require_once dirname(__DIR__) . '/AbstractAnnotations.php';
 require_once dirname(__DIR__) . '/Annotations.php';
-
 require_once __DIR__ . '/stub/Test.php';
