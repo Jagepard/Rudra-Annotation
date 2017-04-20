681cace18dd102336933dac55f718d2a8e9aeb6c
Jagepard <dankorot@gmail.com>
2017-04-20 14:18:05 +0300

    Изменяет docBlock

--- src/tests/stub/PageController.php
+++ src/tests/stub/PageController.php
@@ -4,9 +4,9 @@ declare(strict_types = 1);
 
 /**
  * @Routing(url = '')
- * @Defaults(name='user1'| lastname = 'sample'| age='0'| address = {country : 'Russia', state : 'Tambov'}| phone = '000-00000000')
+ * @Defaults(name='user1', lastname = 'sample', age='0', address = {country : 'Russia'| state : 'Tambov'}, phone = '000-00000000')
  * @assertResult(false)
- * @Validate(name = 'min:150'| phone = 'max:9')
+ * @Validate(name = 'min:150', phone = 'max:9')
  *
  * Class Test
  */
@@ -15,9 +15,9 @@ class PageController
 
     /**
      * @Routing(url = '')
-     * @Defaults(name='user1'| lastname = 'sample'| age='0'| address = {country : 'Russia', state : 'Tambov'}| phone = '000-00000000')
+     * @Defaults(name='user1', lastname = 'sample', age='0', address = {country : 'Russia'| state : 'Tambov'}, phone = '000-00000000')
      * @assertResult(false)
-     * @Validate(name = 'min:150'| phone = 'max:9')
+     * @Validate(name = 'min:150', phone = 'max:9')
      */
     public function indexAction()
     {
