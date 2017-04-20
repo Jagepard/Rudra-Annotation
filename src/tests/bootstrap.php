11f734ece43358e02b85630af4c8b8473c7d40a1
Jagepard <dankorot@gmail.com>
2017-04-20 14:58:31 +0300

    Добавляет vendor/autoload.php

--- src/tests/bootstrap.php
+++ src/tests/bootstrap.php
@@ -1,7 +1,6 @@
 <?php
 
-declare(strict_types = 1);
-
+require_once dirname(dirname(__DIR__)) . '/vendor/autoload.php';
 require_once dirname(__DIR__) . '/AbstractAnnotations.php';
 require_once dirname(__DIR__) . '/Annotations.php';
 require_once __DIR__ . '/stub/PageController.php';
