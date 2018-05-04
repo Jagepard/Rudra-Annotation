8c7d28d50c2396e2501a91d466de1f04ae4d8206
Jagepard <dankorot@gmail.com>
2018-05-04 20:14:54 +0300

    Updated

--- src/tests/bootstrap.php
+++ src/tests/bootstrap.php
@@ -1,6 +1,4 @@
 <?php
 
 require_once dirname(dirname(__DIR__)) . '/vendor/autoload.php';
-require_once dirname(__DIR__) . '/AbstractAnnotations.php';
-require_once dirname(__DIR__) . '/Annotations.php';
 require_once __DIR__ . '/stub/PageController.php';
--- src/tests/stub/PageController.php
+++ src/tests/stub/PageController.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 /**
  * @Routing(url = '')
@@ -8,7 +8,7 @@ declare(strict_types = 1);
  * @assertResult(false)
  * @Validate(name = 'min:150', phone = 'max:9')
  *
- * Class Test
+ * Class PageController
  */
 class PageController
 {
