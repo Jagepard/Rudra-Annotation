50b47343ba6cab861e376ca5e9a6c62bd12d12ad
Jagepard <dankorot@gmail.com>
2019-01-25 17:31:01 +0300

    Delete empty line

--- tests/stub/PageController.php
+++ tests/stub/PageController.php
@@ -12,7 +12,6 @@ namespace Rudra\Tests\Stub;
  */
 class PageController
 {
-
     /**
      * @Routing(url = '')
      * @Defaults(name = 'user1', lastname = 'sample', age='0', address = {country : 'Russia'; state : 'Tambov'}, phone = '000-00000000')
