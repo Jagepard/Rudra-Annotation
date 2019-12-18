92ccf148edf01cdbd931a3b456169f2082d81a20
Jagepard <dankorot@gmail.com>
2019-12-18 10:32:24 +0300

    update use and namespace

--- tests/AnnotationTest.php
+++ tests/AnnotationTest.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 /**
  * @author    : Jagepard <jagepard@yandex.ru">
  * @copyright Copyright (c) 2019, Jagepard
@@ -10,14 +8,11 @@ declare(strict_types=1);
  * phpunit src/tests/ContainerTest --coverage-html src/tests/coverage-html
  */
 
-namespace Rudra\Tests;
+namespace Rudra\Annotation\Tests;
 
 use ReflectionClass;
 use ReflectionMethod;
-use Rudra\Annotation;
-use Rudra\Tests\Stub\PageController;
-use Rudra\Interfaces\ContainerInterface;
-use Rudra\Interfaces\AnnotationInterface;
+use Rudra\Annotation\{Annotation, AnnotationInterface, Tests\Stub\PageController};
 use PHPUnit\Framework\TestCase as PHPUnit_Framework_TestCase;
 
 class AnnotationTest extends PHPUnit_Framework_TestCase
@@ -42,50 +37,44 @@ class AnnotationTest extends PHPUnit_Framework_TestCase
      * @var array
      */
     private $result = [
-        'Routing'      => [['url' => ""]],
-        'Defaults'     => [
+        'Routing' => [['url' => ""]],
+        'Defaults' => [
             [
-                'name'     => "user1",
+                'name' => "user1",
                 'lastname' => "sample",
-                'age'      => "0",
-                'address'  => [
+                'age' => "0",
+                'address' => [
                     'country' => "Russia",
-                    'state'   => "Tambov"
+                    'state' => "Tambov",
                 ],
-                'phone'    => "000-00000000"
-            ]
+                'phone' => "000-00000000",
+            ],
         ],
         'assertResult' => [["false"]],
-        'Validate'     => [
+        'Validate' => [
             [
-                'name'  => "min:150",
-                'phone' => "max:9"
-            ]
+                'name' => "min:150",
+                'phone' => "max:9",
+            ],
         ],
-        'Middleware'   => [
+        'Middleware' => [
             [
-                0        => "'Middleware'",
+                0 => "'Middleware'",
                 'params' => [
-                    'int1' => '123'
-                ]
-            ]
-        ]
+                    'int1' => '123',
+                ],
+            ],
+        ],
     ];
 
     protected function setUp(): void
     {
-        rudra()->setBinding(ContainerInterface::class, rudra());
-        $this->annotation = rudra()->new(Annotation::class);
+        $this->annotation = new Annotation();
     }
 
-    /**
-     * @param string $name
-     * @return ReflectionMethod
-     * @throws \ReflectionException
-     */
     private function getMethod(string $name): ReflectionMethod
     {
-        $class  = new ReflectionClass($this->annotation);
+        $class = new ReflectionClass($this->annotation);
         $method = $class->getMethod($name);
         $method->setAccessible(true);
 
@@ -105,9 +94,6 @@ class AnnotationTest extends PHPUnit_Framework_TestCase
 
     public function testGetMethodAnnotations(): void
     {
-        $this->assertEquals($this->result, $this->annotation->getAnnotations(
-            PageController::class,
-            'indexAction'
-        ));
+        $this->assertEquals($this->result, $this->annotation->getAnnotations(PageController::class, 'indexAction'));
     }
 }
