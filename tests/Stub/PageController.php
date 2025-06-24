d90e193179303de69f9dbb868d8f850a49a2e203
Jagepard <jagepard@yandex.ru>
2025-06-24 14:42:30 +0300

    add @Annotation

--- tests/Stub/PageController.php
+++ tests/Stub/PageController.php
@@ -8,15 +8,17 @@ namespace Rudra\Annotation\Tests\Stub;
  * @assertResult(false)
  * @Validate(name = 'min:150', phone = 'max:9')
  * @Middleware('Middleware', params = {int1 : '123'})
+ * @Annotation(param1, param2 = 'param2', param3={param1;param2:'param2'})
  */
 class PageController
 {
     /**
      * @Routing(url = '')
-     * @Defaults(name = 'user1', lastname = 'sample', age='0', address = {country : 'Russia'; state : 'Tambov'}, phone = '000-00000000')
+     * @Defaults(name='user1', lastname = 'sample', age='0', address={country : 'Russia'; state : 'Tambov'}, phone = '000-00000000')
      * @assertResult(false)
      * @Validate(name = 'min:150', phone = 'max:9')
      * @Middleware('Middleware', params = {int1 : '123'})
+     * @Annotation(param1, param2 = 'param2', param3={param1;param2:'param2'})
      */
     public function indexAction(): void
     {
@@ -27,6 +29,7 @@ class PageController
     #[assertResult('false')]
     #[Validate(name:'min:150', phone:'max:9')]
     #[Middleware("'Middleware'", params:['int1' => '123'])]
+    #[Annotation("param1", param2:'param2', param3:['param1', 'param2' => 'param2'])]
     public function secondAction()
     {
     }
