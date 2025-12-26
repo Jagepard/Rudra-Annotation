69ca94810fa5b7f06c28321f2bd2a50ba12f1113
Jagepard <jagepard@yandex.ru>
2025-12-26 14:42:39 +0300

    fix

--- src/AnnotationInterface.php
+++ src/AnnotationInterface.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 /**
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -10,7 +8,6 @@ declare(strict_types=1);
  * @author  Korotkov Danila (Jagepard) <jagepard@yandex.ru>
  * @license https://mozilla.org/MPL/2.0/  MPL-2.0
  */
-
 
 namespace Rudra\Annotation;
 
